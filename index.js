ff7808c202beda41bf7c38816b99655bb1e3d6ae
justinmarkhamlawrence <justinmarkhamlawrence@gmail.com>
2016-10-26 08:44:29 -0400

    yelp api functional, dotenv functional

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as server from './server/index';
 import * as client from './client/index';
-
+var dotenv = require('dotenv');
+dotenv.load();
 export {
     server,
     client
--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
 import 'babel-polyfill';
 import express from 'express';
+var dotenv = require('dotenv');
+var request = require('request');
+var Yelp = require('yelp');
+
+dotenv.load();
+var yelp = new Yelp({
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  token: process.env.token,
+  token_secret: process.env.token_secret,
+});
 
 const HOST = process.env.HOST;
 const PORT = process.env.PORT || 8080;
@@ -10,6 +21,21 @@ const app = express();
 
 app.use(express.static(process.env.CLIENT_PATH));
 
+app.get('/api/:term/:location', function(req, res){
+  let term = req.params.term;
+  let location = req.params.location;
+  yelp.search({ term: term,
+   location: location,
+   sort: '1', limit: '3', radius_filter:'2000'})
+  .then(function (data) {
+    // console.log(data)
+    return res.send(data)
+   })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+  
 function runServer() {
     return new Promise((resolve, reject) => {
         app.listen(PORT, HOST, (err) => {
